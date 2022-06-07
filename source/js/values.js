a8a0cf07eb8c1c63eb742c5e3857ced24b540eb1
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2022-06-07 14:25:38 +0300

    Benefits animation

--- source/js/values.js
+++ source/js/values.js
@@ -1,10 +1,10 @@
-const btns = document.querySelectorAll('.values__more');
+const valuesBtns = document.querySelectorAll('.values__more');
 
-const handleClick = ( evt ) => {
-  const btn =  evt.target;
-  btn.closest( '.values__item').classList.toggle( 'active' );
+const valuesHandleClick = (evt) => {
+  const btn = evt.target;
+  btn.closest('.values__item').classList.toggle('active');
 }
 
-btns.forEach( btn => {
-  btn.addEventListener( 'click', handleClick )
+valuesBtns.forEach(btn => {
+  btn.addEventListener('click', valuesHandleClick)
 })
