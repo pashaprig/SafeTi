c841972626e4b6d8d5a4654e91f7105ea6ebf6d2
Pavlo Pryharnytskyi <pashaprig@gmail.com>
2022-06-08 22:55:52 +0300

    Call mobile/desktop layout + form padding + ecosystem mobile/desctop layout

--- source/js/slider.js
+++ source/js/slider.js
@@ -1,7 +1,16 @@
-$('.info__slider').slick({
-    dots: true,
-    infinite: true,
-    // autoplay:true,
-    speed: 300,
-    arrows: false,
+$('#info__slider').slick({
+  dots: true,
+  infinite: true,
+  // autoplay:true,
+  speed: 300,
+  arrows: false,
+});
+
+$('#call__slider').slick({
+  infinite: true,
+  autoplay: true,
+  speed: 2000,
+  arrows: false,
+  fade: true,
+  zIndex: 0,
 });
